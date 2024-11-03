82a808b3fb38f813f4d6401d9cb78376dc9cacef
Alan Mathew <alanmathew0891@gmail.com>
2024-11-03 13:20:46 +0530

    cleaning some codes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,6 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SendComponent } from './pages/send/send.component';
-import { RecieveComponent } from './pages/recieve/recieve.component';
-import { TransactionListComponent } from './pages/transaction-list/transaction-list.component';
 import { authGuard, loginGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
@@ -20,21 +18,6 @@ export const routes: Routes = [
     {
         path: 'dashboard',
         component: DashboardComponent,
-        canActivate: [authGuard]
-    },
-    {
-        path: 'send-money',
-        component: SendComponent,
-        canActivate: [authGuard]
-    },
-    {
-        path: 'recieve-money',
-        component: RecieveComponent,
-        canActivate: [authGuard]
-    },
-    {
-        path: 'transactions',
-        component: TransactionListComponent,
         canActivate: [authGuard]
     },
     {
